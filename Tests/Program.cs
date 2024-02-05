21170bcc8b9c12995e88f7cbc2058ab53b70061f
eduardo.worrel <eduardo.worrel@gmail.com>
2024-02-04 23:39:41 -0300

    feat: :rocket: configurando test de carga para executar com dotnet run

---
+++ Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+ProcessStartInfo startInfo = new ProcessStartInfo
+{
+    FileName = "bash",
+    Arguments = "./executar-teste-local.sh",
+    RedirectStandardOutput = false,
+    RedirectStandardError = false,
+    UseShellExecute = false,
+    CreateNoWindow = false
+};
+using Process processo = new() { StartInfo = startInfo };
+
+processo.Start();
+
+Console.ReadLine();
